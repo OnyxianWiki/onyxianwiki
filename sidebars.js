e178659a2d1b14180886f49414d3f506090f3d23
Uriel Salvio <salvio.uriel@gmail.com>
2021-04-10 21:25:35 -0300

    Attempted to fix a sidebar issue

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,6 @@
 module.exports = {
   someSidebar: {
     Plugins: ['OnyxianCore/doc1'],
-    Plugins2: ['/OnyxianCore/doc1'],
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['mdx'],
   },
